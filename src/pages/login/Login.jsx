fbae289f559abed46cf9ca83647d2d3fba652176
leocodes-al <leonardo.sistemainformacao@gmail.com>
2026-05-10 09:15:58 -0300

    Atualizações login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,36 @@
+import logoLogin from "../../assets/logo-icon.png"
+import gmail from "../../assets/logoGoogle.png"
+
+
 import "./Login.css"
 
 function Login() {
   return (
-    <section>
-      <h1>Login</h1>
-    </section>
+
+    <main className="login">
+      <div className="container-login">
+
+        <div className="logo-titulo">
+          <img src={logoLogin} alt="logo orbita" />
+          <h3>Faça login ou crie uma conta</h3>
+        </div>
+
+        <div className="acesso">
+          <label>E-mail</label>
+          <input type="text" placeholder="Insira seu e-mail"/>
+          <button>Continuar com E-mail</button>
+        </div>
+
+        <div className="opcoes-acesso">
+          <label>ou use uma das seguintes opções</label>
+          <button>
+            <img src={gmail} alt="gmail google" />
+          </button>
+          <p>Perdeu o acesso a conta? <span>AJUDA</span></p>
+        </div>
+
+      </div>
+    </main>
   )
 }
 
